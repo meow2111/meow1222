17d21228c48c3c47cd9bc1b0622d2068730f5cc1
meow2111 <potemkinasofa11@gmail.com>
2025-11-27 19:01:41 +0300

    Add files via upload

---
+++ practica16/task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    public class Score
+    {
+        private int math;
+        private int russian;
+        private int physics;
+
+        public int this[string subject]
+        {
+            get
+            {
+                if (subject == "Математика")
+                    return math;
+                else if (subject == "Русский")
+                    return russian;
+                else if (subject == "Физика")
+                    return physics;
+                else
+                    return 0;
+            }
+            set
+            {
+                if (subject == "Математика")
+                    math = value;
+                else if (subject == "Русский")
+                    russian = value;
+                else if (subject == "Физика")
+                    physics = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Математика: " + math + ", Русский: " + russian + ", Физика: " + physics;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Score();
+
+            s["Математика"] = 5;
+            s["Русский"] = 4;
+
+            Console.WriteLine(s["Математика"]);
+            Console.WriteLine(s);
+        }
+    }
+}
